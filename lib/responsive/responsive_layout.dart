3970483a6321cd2d0153d7ec91e24b6246310e13
BharaT-Coderr <bharatsarkar66666@gmail.com>
2023-01-25 15:29:13 +0530

    base

---
+++ lib/responsive/responsive_layout.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobileScreenLayout;
+  final Widget webScreenLayout;
+
+  const ResponsiveLayout({
+    Key? key,
+    required this.mobileScreenLayout,
+    required this.webScreenLayout,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 938) {
+          return webScreenLayout;
+        }
+
+        return mobileScreenLayout;
+      },
+    );
+  }
+}
