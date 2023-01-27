4260ede88d196982cae97482c311c5247b1dd7d2
BharaT-Coderr <bharatsarkar66666@gmail.com>
2023-01-27 16:26:45 +0530

    firebase setup is done

---
+++ lib/features/landing/screens/landing_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatelessWidget {
+  const LandingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
